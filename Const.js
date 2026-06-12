775f2dfa06895404193f6c4580316fd80badba0a
malathy241200 <malathy241200@gmail.com>
2026-06-12 11:36:28 +0530

    Add files via upload
    "JS"

---
+++ Const.js
@@ -0,0 +1,47 @@
+console.log("external Js")
+const a=10;
+const b=20;
+console.log(a+b)
+const str="String"
+console.log(str)
+
+const arr=[1,2.1,"str",false]
+console.log(arr)
+
+arr[1]= 3.1
+console.log(arr[1])
+
+arr.push(4)
+console.log(arr)
+
+arr.pop()
+console.log(arr)
+
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i])
+}
+
+//functios
+//syntax
+//function fn_name(){
+///}
+
+function example(){
+    console.log("i am writting")
+
+}
+const access=document.getElementById("ptag")
+access.innerHTML="paragraph"
+
+const input=document.getElementById("input")
+const output=document.getElementById("inputField")
+
+input.addEventListener("input",()=>{
+    output.textContent=input.value
+})
+
+const first = document.createElement("h1")
+first.textContent="Malathy"
+document.body.appendChild(first)
+//to remove an element
+//first.remove()
